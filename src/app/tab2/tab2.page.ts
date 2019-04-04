10a289b64b945ae92295d7bba42bae49ac54216a
Tudor_Shiba <tudor.barbu7@gmail.com>
2019-04-04 13:47:38 +1100

    Added ionic code to tryout video conferencing

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,4 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
+/*
+
+var connection = new RTCMultiConnection();
+
+// this line is VERY_important
+connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+
+// all below lines are optional; however recommended.
+
+connection.session = {
+    audio: true,
+    video: true
+};
+
+connection.sdpConstraints.mandatory = {
+    OfferToReceiveAudio: true,
+    OfferToReceiveVideo: true
+};
+
+connection.onstream = function(event) {
+    document.body.appendChild( event.mediaElement );
+};
+
+*/
 export class Tab2Page {}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,75 @@
 import { Component } from '@angular/core';
+import * as io from 'socket.io-client';
+import * as RTCMultiConnection from 'rtcmulticonnection';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+	connection: any = null;
+	predefinedRoomId: string = "1234xxxQ";
+
+	initVideoCall(){
+
+        console.log("Init Call");
+
+	    this.connection = new RTCMultiConnection();
+	    var connection = this.connection;
+
+	    // this line is VERY_important
+	    connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+
+	    // if you want audio+video conferencing
+	    connection.session = {
+	        audio: true
+	    };
+
+	    connection.onMediaError = function (error) {
+	    	console.log("Media error encountered.");
+
+	    	console.log(error.name);
+	    	console.log(error.message);
+		    // error.name == 'PermissionDeniedError' etc.
+		    // error.message
+		    // error.constraintName
+		    // error.session --- original session that is used to capture media
+		}
+
+		/*	    
+		connection.captureUserMedia(function(camera) {
+		    var video = document.createElement('video');
+		    video.src = URL.createObjectURL(camera);
+		    video.muted = true;
+
+		    var streamEvent = {
+		        type: 'local',
+		        stream: camera,
+		        streamid: camera.id,
+		        mediaElement: video
+		    };
+		    connection.onstream(streamEvent);
+		}, {audio:true});
+		*/
+	 	connection.openOrJoin(this.predefinedRoomId);
+
+	 	let videoContainer = document.getElementById('webRTC-video-me');
+	 	videoContainer.style.backgroundColor = "red";
+
+	 	connection.videosContainer = videoContainer;
+
+	 	
+    }
+
+    openRoom(){
+        console.log("Open Room Clicked");
+        this.connection.open( this.predefinedRoomId );
+    }
+
+    joinRoom(){
+        console.log("Join Room Clicked");
+        this.connection.join( this.predefinedRoomId );
+    }
+}
